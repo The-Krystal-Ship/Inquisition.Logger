4f46f27afde5c5b25ee7575d37250526d1e466e8
Cristian Moraru <cristian.moraru@live.com>
2018-06-18 19:36:17 +0200

    Added IServiceCollection extension

---
+++ Inquisition.Logging/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Inquisition.Logging.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddLogger(this IServiceCollection services)
+        {
+            services.TryAdd(ServiceDescriptor.Singleton(typeof(ILogger<>), typeof(Logger<>)));
+            return services;
+        }
+    }
+}
